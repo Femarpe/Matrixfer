8ba6aebe8ad17eef6b55a8d52be92aaab8f11ebe
kurur <kururu.fm@gmail.com>
2020-01-13 22:11:09 +0100

    2 PERSONAJE

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,65 +4,105 @@ import maths.dam.Matrix2D;
 
 public class Game {
 
-    public void startGame(){
-
-        Matrix2D m = new Matrix2D(1000, 1000);
-        int bg = Canvas.toARGB(255, 255, 255, 255);
-        int fg = Canvas.toARGB(255, 255, 255, 0);
-        int fg2 = Canvas.toARGB(255, 255, 255, 255);
-        int fg3 = Canvas.toARGB(255, 0, 0, 0);
+    public void startGame() {
         Canvas canvas = new Canvas();
+        Matrix2D m = new Matrix2D(1000, 1000);
+        int background = Canvas.toARGB(255, 255, 255, 255);
+        canvas.clear(m, background);
 
+
+        int velocidadjugador = 10;
         int x = 0;
         int xojo = 0;
         int y = 0;
         int yojo = 0;
+
+        int velocidadGato = 7;
+        int mx = 0;
+        int my = 0;
+        int mxojo = 0;
+        int myojo = 0;
         boolean xt = true;
+
+
+        Personaje p = new Personaje();
+
+
         while (true) {
-            canvas.clear(m, bg);
-            canvas.fillCircle(m, fg, 150 + x, 150 + y, 100);
-            canvas.fillCircle(m, fg2, 120+ x, 100 + y, 25);
-            canvas.fillCircle(m, fg2, 180+ x, 100 + y, 25);
-            canvas.fillCircle(m, fg3, 120+ xojo, 100 + yojo, 10);
-            canvas.fillCircle(m, fg3, 180+ xojo, 100 + yojo, 10);
-            canvas.fillRectangle(m, fg2, 135+ x, 135 + y, 30, 15);
-            canvas.fillRectangle(m, fg2, 135+ x, 175 + y, 30, 15);
-            canvas.fillRectangle(m, fg2, 100+ x, 175 + y, 30, 15);
-            canvas.fillRectangle(m, fg2, 170+ x, 175 + y, 30, 15);
-            canvas.fillRectangle(m, fg2, 70+ x, 160 + y, 30, 15);
-            canvas.fillRectangle(m, fg2, 200+ x, 160 + y, 30, 15);
-            if (x == 700){
-                x-=5;
-                xojo = x - 5;
+            canvas.clear(m, background);
+
+            /**jugador*/
+            //p.pintarCarax(m,x,y,xojo,yojo);
+            p.pintarCaraRaton(m, x, y, xojo, yojo);
+
+
+            if (x <= 0) {
+                x += velocidadjugador;
+                xojo = x + velocidadjugador;
+            } else if (dam.c1k.Window.isPressedKey(65) == true) {
+                x -= velocidadjugador;
+                xojo = x - velocidadjugador;
+            }
+
+            if (x >= 700) {
+                x -= velocidadjugador;
+                xojo = x - velocidadjugador;
             } else if (dam.c1k.Window.isPressedKey(68) == true) {
-                x+=5;
-                xojo = x + 5;
+                x += velocidadjugador;
+                xojo = x + velocidadjugador;
             }
 
-            if (x == 0){
-                x+=5;
-                xojo = x + 5;
-            } else if (dam.c1k.Window.isPressedKey(65) == true){
-                x-=5;
-                xojo = x - 5;
+            if (y <= 0) {
+                y += velocidadjugador;
+                yojo = y + velocidadjugador;
+            } else if (dam.c1k.Window.isPressedKey(87) == true) {
+                y -= velocidadjugador;
+                yojo = y - velocidadjugador;
             }
 
-            if (y == 0){
-                y+=5;
-                yojo = y + 5;
-            } else if (dam.c1k.Window.isPressedKey(87) == true){
-                y-=5;
-                yojo = y - 5;
-            }
-
-            if (y == 700){
-                y-=5;
-                yojo = y - 5;
+            if (y >= 700) {
+                y -= velocidadjugador;
+                yojo = y - velocidadjugador;
             } else if (dam.c1k.Window.isPressedKey(83) == true) {
-                y+=5;
-                yojo = y + 5;
+                y += velocidadjugador;
+                yojo = y + velocidadjugador;
             }
 
+            /**Gato*/
+
+            p.pintarCaraMalGato(m, mx, my, mxojo, myojo);
+
+            if (mx <= -700) {
+                mx += velocidadGato;
+                mxojo = mx + velocidadGato;
+            } else if (dam.c1k.Window.isPressedKey(263) == true) {
+                mx -= velocidadGato;
+                mxojo = mx - velocidadGato;
+            }
+
+            if (mx >= 0) {
+                mx -= velocidadGato;
+                mxojo = x - velocidadGato;
+            } else if (dam.c1k.Window.isPressedKey(262) == true) {
+                mx += velocidadGato;
+                mxojo = mx + velocidadGato;
+            }
+
+            if (my <= -700) {
+                my += velocidadGato;
+                myojo = my + velocidadGato;
+            } else if (dam.c1k.Window.isPressedKey(265) == true) {
+                my -= velocidadGato;
+                myojo = my - velocidadGato;
+            }
+
+            if (my >= 0) {
+                my -= velocidadGato;
+                myojo = my - velocidadGato;
+            } else if (dam.c1k.Window.isPressedKey(264) == true) {
+                my += velocidadGato;
+                myojo = my + velocidadGato;
+            }
 
             m.swapP();
             dam.c1k.Window.showImage(m.getWidth(), m.getHeight(), m.getValues());
--- src/main/java/Personaje.java
+++ src/main/java/Personaje.java
@@ -1,5 +1,63 @@
+import graphics.dam.Canvas;
+import maths.dam.Matrix2D;
+
 public class Personaje {
     int vida = 10;
     int danyo = 5;
 
+    int amarillo = Canvas.toARGB(255, 255, 255, 0);
+    int blanco = Canvas.toARGB(255, 255, 255, 255);
+    int rojo = Canvas.toARGB(250, 255, 0, 0);
+    int negro = Canvas.toARGB(255, 0, 0, 0);
+    int gris = Canvas.toARGB(255, 100, 100, 100);
+
+    Canvas canvas = new Canvas();
+
+    public void pintarCarax (Matrix2D m, int x, int y, int xojo, int yojo ) {
+
+        canvas.fillCircle(m, amarillo, 150 + x, 150 + y, 100);
+        canvas.fillCircle(m, blanco, 120 + x, 100 + y, 25);
+        canvas.fillCircle(m, blanco, 180 + x, 100 + y, 25);
+        canvas.fillCircle(m, negro, 120 + xojo, 100 + yojo, 10);
+        canvas.fillCircle(m, negro, 180 + xojo, 100 + yojo, 10);
+        canvas.fillRectangle(m, blanco, 135 + x, 135 + y, 30, 15);
+        canvas.fillRectangle(m, blanco, 135 + x, 175 + y, 30, 15);
+        canvas.fillRectangle(m, blanco, 100 + x, 175 + y, 30, 15);
+        canvas.fillRectangle(m, blanco, 170 + x, 175 + y, 30, 15);
+        canvas.fillRectangle(m, blanco, 70 + x, 160 + y, 30, 15);
+        canvas.fillRectangle(m, blanco, 200 + x, 160 + y, 30, 15);
+    }
+
+    public void pintarCaraRaton (Matrix2D m, int x, int y, int xojo, int yojo ) {
+
+
+        canvas.fillCircle(m, gris, 150 + x, 150 + y, 60);
+        canvas.fillCircle(m, gris, 120 + x, 100 + y, 30);
+        canvas.fillCircle(m, gris, 180 + x, 100 + y, 30);
+        canvas.fillCircle(m, blanco, 120 + x, 100 + y, 25);
+        canvas.fillCircle(m, blanco, 180 + x, 100 + y, 25);
+        canvas.fillCircle(m, negro, 120 + xojo, 100 + yojo, 10);
+        canvas.fillCircle(m, negro, 180 + xojo, 100 + yojo, 10);
+        canvas.fillRectangle(m, negro, 135 + x, 135 + y, 30, 15);
+        canvas.fillRectangle(m, blanco, 135 + x, 175 + y, 30, 15);
+        canvas.fillRectangle(m, blanco, 100 + x, 160 + y, 30, 15);
+        canvas.fillRectangle(m, blanco, 170 + x, 160 + y, 30, 15);
+    }
+
+    public void pintarCaraMalGato(Matrix2D m, int mx, int my, int mxojo, int myojo ) {
+
+
+        canvas.fillCircle(m, rojo, 850 + mx, 850 + my, 100);
+        canvas.fillCircle(m, blanco, 820 + mx, 800 + my, 25);
+        canvas.fillCircle(m, blanco, 880 + mx, 800 + my, 25);
+        canvas.fillCircle(m, negro, 820 + mxojo, 800 + myojo, 10);
+        canvas.fillCircle(m, negro, 880 + mxojo, 800 + myojo, 10);
+        canvas.fillRectangle(m, blanco, 835 + mx, 835 + my, 30, 15);
+        canvas.fillRectangle(m, negro, 835 + mx, 845 + my, 30, 15);
+        canvas.fillRectangle(m, blanco, 835 + mx, 875 + my, 30, 15);
+        canvas.fillRectangle(m, blanco, 800 + mx, 875 + my, 30, 15);
+        canvas.fillRectangle(m, blanco, 870 + mx, 875 + my, 30, 15);
+        canvas.fillRectangle(m, blanco, 770 + mx, 860 + my, 30, 15);
+        canvas.fillRectangle(m, blanco, 900 + mx, 860 + my, 30, 15);
+    }
 }
